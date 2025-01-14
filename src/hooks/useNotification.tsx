0108d5c833f4fcc05fa55b0c089a426e745bf8c4
mobin afshari <mma137813781378@gmail.com>
2025-01-14 17:43:53 +0330

    fix: animation implementing fixed

--- src/hooks/useNotification.tsx
+++ src/hooks/useNotification.tsx
@@ -6,6 +6,7 @@ type Props = {
   text: string;
   type?: "warn" | "error" | "success" | "info";
   duration?: number;
+  animationDuration?: number;
 };
 
 const colors = {
@@ -14,34 +15,49 @@ const colors = {
   error: "#FF748B",
   info: "white",
 };
+
 const useNotification = ({
   type = "success",
   text,
   duration = 5000,
+  animationDuration = 1000,
 }: Props) => {
   const [opening, setSituation] = useState(true);
-  console.log(opening)
+  const [visible, setVisible] = useState(true);
+
   useEffect(() => {
-    setTimeout(() => {
+    const timeoutId = setTimeout(() => {
       setSituation(false);
+
+      const removeTimeoutId = setTimeout(() => {
+        setVisible(false);
+      }, animationDuration);
+
+      return () => clearTimeout(removeTimeoutId);
     }, duration);
-  } , [duration])
+
+    return () => clearTimeout(timeoutId);
+  }, [duration, animationDuration]);
+
   const notificationColor = colors[type];
+
   const Toast = useCallback(
-    () => (
-      <section
-        className={`notification ${
-          opening ? "notification-open" : "notification-close"
-        }`}
-        style={{
-          backgroundColor: notificationColor,
-          borderColor: colord(notificationColor).darken(0.3).toHex(),
-        }}>
-        {text}
-      </section>
-    ),
-    [opening]
+    () =>
+      visible && (
+        <section
+          className={`notification ${
+            opening ? "notification-open" : "notification-close"
+          }`}
+          style={{
+            backgroundColor: notificationColor,
+            borderColor: colord(notificationColor).darken(0.3).toHex(),
+          }}>
+          {text}
+        </section>
+      ),
+    [opening, visible, notificationColor, text]
   );
+
   return Toast;
 };
 
