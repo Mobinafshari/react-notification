95dc610522c605be9e023b5069c6776b14bf1cfe
mobin afshari <mma137813781378@gmail.com>
2025-01-16 11:55:21 +0330

    fix: Notification.text.tsx is complete now and everything is ok

--- test/Notification.test.tsx
+++ test/Notification.test.tsx
@@ -1,11 +1,23 @@
 import { render, screen } from '@testing-library/react'
 import App from "../src/App";
+import userEvent from "@testing-library/user-event";
 
 describe("notification", () => {
-  it("should", () => {
+  it("should have button for trigger notification", () => {
     render(<App />);
     const trigger = screen.getByRole("button");
-    // const notification = screen.getByRole("alert");
     expect(trigger).toBeInTheDocument()
+  });
+  it("should not have toast", () => {
+    render(<App />);
+    const notification = screen.queryByRole("alert");
+    expect(notification).not.toBeInTheDocument()
+  });
+  it("trigger toast", async() => {
+    render(<App />);
+    const trigger = screen.getByRole("button");
+    const user = userEvent.setup()
+    await user.click(trigger);
+    expect(screen.getByRole("alert")).toBeInTheDocument();
   });
 });
